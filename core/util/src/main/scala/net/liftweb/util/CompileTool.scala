11d6be77f27894e53dd129502959706ff82f00ac
David Pollak <feeder.of.the.bears@gmail.com>
2012-10-18 16:55:09 -0700

    Got the class compiler/class loader util stuff working

--- core/util/src/main/scala/net/liftweb/util/CompileTool.scala
+++ core/util/src/main/scala/net/liftweb/util/CompileTool.scala
@@ -2,12 +2,10 @@ package net.liftweb
 package util
 
 import common._
-import tools.nsc.{Global, CompilerCommand, Settings}
-import tools.nsc.reporters.ConsoleReporter
-import tools.nsc.interactive.Response
-import tools.nsc.io.AbstractFile
-import java.io.{InputStream, File, OutputStream}
-import reflect.internal.util.SourceFile
+import tools.nsc.reporters.Reporter
+import collection.mutable.ListBuffer
+import java.util.concurrent.ConcurrentHashMap
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,23 +15,117 @@ import reflect.internal.util.SourceFile
  *
  * A set of tools for compiling text into Scala code
  */
+
+
 class CompileTool {
 
-  private def captureError(err: String) {
+  import java.io.File
+  import scala.tools.nsc.Global
+  import scala.reflect.internal.util.{SourceFile, Position, BatchSourceFile}
+  import scala.tools.nsc.io.{VirtualDirectory, AbstractFile}
+  import scala.tools.nsc.Settings
+  import scala.tools.nsc.reporters.ConsoleReporter
 
+
+  def compiler(vd: AbstractFile, reporter: Reporter) = {
+    val settings = new Settings
+
+    settings.outputDirs.setSingleOutput(vd)
+
+    val wheresScala = Class.forName("scala.ScalaObject").getProtectionDomain.getCodeSource
+    val wheresCompiler = Class.forName("scala.tools.nsc.Interpreter").getProtectionDomain.
+      getCodeSource.getLocation.getFile
+    val wheresLibs = wheresScala.getLocation.getFile
+
+    val path = List(wheresCompiler, wheresLibs)
+    val settingsClasspath = settings.bootclasspath.value
+    settings.bootclasspath.value = (settingsClasspath :: path).mkString(File.pathSeparator)
+
+
+    val compiler = new Global(settings, reporter)
+
+    compiler
   }
 
-  def compile(in: Seq[(String, String)]): Box[Seq[Class[_]]] = {
-    val settings = new Settings(captureError)
-    val reporter = new ConsoleReporter(settings)
-    val command = new CompilerCommand(Nil, settings)
-    object compiler extends Global(command.settings, reporter) {
-      // printTypings = true
+
+  private def fixPath(str: String): String = {
+    val one = str.substring("(memory)/".length)
+    val two = one.substring(0, one.length - 6)
+    two.replace('/', '.')
+  }
+
+  def fileToBytes(in: AbstractFile): List[(String, Array[Byte])] =
+    if (in.isDirectory) in.iterator.toList.flatMap(fileToBytes(_))
+    else List(fixPath(in.canonicalPath) -> in.toByteArray)
+
+  def classloaderFor(src: Seq[(String, String)], oldClasses: Map[String, Array[Byte]] = Map.empty): Box[ClassLoader] = {
+    for {
+      newMap <- compile(src)
+      map = newMap.toList.foldLeft(oldClasses){case (mip, nb) => mip + nb }
+      masterCL = this.getClass.getClassLoader
+    } yield new ClassLoader(masterCL) {
+      private val classes: ConcurrentHashMap[String, Class[_]] = new ConcurrentHashMap()
+
+      override def loadClass(className: String): Class[_] = {
+        map.get(className) match {
+          case Some(_) => findClass(className)
+          case _ => masterCL.loadClass(className)
+        }
+      }
+
+      override def findClass(className: String): Class[_] = {
+        classes.get(className) match {
+          case null =>
+            try {
+              val ret = findSystemClass(className)
+              ret
+            } catch {
+              case e: Exception =>
+                map.get(className) match {
+                  case Some(bytes) =>
+                    val ret = defineClass(className, bytes, 0, bytes.length, null)
+                    classes.put(className, ret)
+                    ret
+                  case _ => null
+
+                }
+            }
+          case c => c
+        }
+
+      }
+    }
+  }
+
+  def compile(src: Seq[(String, String)]): Box[Map[String, Array[Byte]]] = {
+    val virtualDirectory = new VirtualDirectory("(memory)", None)
+
+    val errors: ListBuffer[(Position, String)] = new ListBuffer[(Position, String)]
+
+    val reporter = new Reporter {
+      protected def info0(pos: Position, msg: String, severity: this.type#Severity, force: Boolean) {
+        severity match {
+          case this.ERROR => errors.append(pos -> msg)
+          case _ =>
+        }
+      }
     }
 
-    // new SourceFile {}
-    (new compiler.Run).compileSources(Nil)
 
-    Empty
+    val comp = compiler(virtualDirectory, reporter)
+
+    val files = src.toList.map {
+      case (name, code) => new BatchSourceFile(name, code)
+    }
+
+    val run = new comp.Run
+    run.compileSources(files)
+
+    errors.toList match {
+      case Nil => Full(Map(fileToBytes(virtualDirectory): _*))
+      case errs => ParamFailure(errs.mkString((", ")), errs)
+    }
   }
+
+
 }
--- core/util/src/test/scala/net/liftweb/util/CompileToolSpec.scala
+++ core/util/src/test/scala/net/liftweb/util/CompileToolSpec.scala
@@ -3,7 +3,7 @@ package net.liftweb
 package util
 
 import org.specs2.mutable.Specification
-import common.Empty
+import common._
 
 
 /**
@@ -12,15 +12,71 @@ import common.Empty
 object CompileToolSpec extends Specification  {
   "CompileTool Specification".title
 
+  def example = """
+     class Mook {
+         def moose = 1 + 1
+     }
+
+     class Foo {
+         def bar() = "Hello World" // you snarky weasle
+     }
+                """
+
+  def badExample =
+    """
+      |class DogMeat {
+      |  def moose 99 frog = "hello"
+      |}
+    """.stripMargin
+
+  def futureExample =
+    """
+      |
+      |
+      |import java.util.concurrent._
+      |
+      |case class Meow() extends Future[String] {
+      |  def cancel(mayInterruptIfRunning: Boolean): Boolean = true
+
+      |  def get(timeout: Long, unit: TimeUnit): String = get()
+      |  def isCancelled() = false
+      |  def isDone() = true
+      |  def get() = "44"
+      |}
+    """.stripMargin
+
+  def funcExample =
+    """
+      |case class Meow() extends Function1[Int, String] {
+      |  def apply(in: Int) = in.toString
+      |}
+    """.stripMargin
+
   "the CompileTools class" should {
     "Compile a source file" in {
-      try {
       val ct = new CompileTool
-      ct.compile(Nil) must_== Empty
-      } catch {
-        case e: Exception => e.printStackTrace()
-      }
-      1 must_== 1
+      ct.compile(List("Frog.scala" -> example)).map(_.size) must_== Full(2)
+    }
+
+    "Fail to compile a bad source" in {
+      val ct = new CompileTool
+      ct.compile(List("Frog.scala" -> badExample)).isEmpty must_== true
+    }
+
+    "Load a class" in {
+      val ct = new CompileTool
+      this.getClass.getClassLoader.loadClass("scala.Product$class")
+      val cl = ct.classloaderFor(List("Frog.scala" -> futureExample)).openOrThrowException("The code should compile")
+      val inst = cl.loadClass("Meow").newInstance().asInstanceOf[java.util.concurrent.Future[String]]
+      inst.get() must_== "44"
+    }
+
+    "Load a function class" in {
+      val ct = new CompileTool
+      this.getClass.getClassLoader.loadClass("scala.Product$class")
+      val cl = ct.classloaderFor(List("Frog.scala" -> funcExample)).openOrThrowException("The code should compile")
+      val inst = cl.loadClass("Meow").newInstance().asInstanceOf[Function1[Int, String]]
+      inst(55) must_== "55"
     }
   }
 }
