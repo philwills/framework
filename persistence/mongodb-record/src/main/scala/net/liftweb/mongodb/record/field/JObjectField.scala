46bbac210860c82c267bf4d6de2c04076d578389
Tim Nelson <tim@eltimn.com>
2012-11-30 14:10:05 -0600

    JObjectField doesn't serialize to BSON

--- persistence/mongodb-record/src/main/scala/net/liftweb/mongodb/record/field/JObjectField.scala
+++ persistence/mongodb-record/src/main/scala/net/liftweb/mongodb/record/field/JObjectField.scala
@@ -29,7 +29,7 @@ import scala.xml.NodeSeq
 
 import com.mongodb._
 
-class JObjectField[OwnerType <: BsonRecord[OwnerType]](rec: OwnerType)
+abstract class JObjectField[OwnerType <: BsonRecord[OwnerType]](rec: OwnerType)
 extends Field[JObject, OwnerType]
 with MandatoryTypedField[JObject]
 with MongoFieldFlavor[JObject] {
